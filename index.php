79d43e504c658d8adc600b120c85d8e43ca725c3
Saikat Mukhopadhyay <mukhopadhyay.saikat@gmail.com>
2017-09-22 22:49:14 -0600

    Add New File

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+   <body>
+      
+      <?php
+         $books = array( 
+            "PHP" => array (
+               "Name" => "Learning PHP",
+               "ID" => 30,	
+               "Level" => "Basic"
+            ),
+            
+            "Java" => array (
+               "Name" => "Java Basic",
+               "ID" => 40,	
+               "Level" => "intermediate"
+            ),
+         );
+         
+         /* Accessing multi-dimensional array values */
+         echo "Name for PHP Book: " ;
+         echo $books['PHP']['Name'] . "<br />"; 
+         
+         echo "ID for PHP Book: " ;
+         echo $books['PHP']['ID'] . "<br />"; 
+         
+         echo "Level for Java Book: " ;
+         echo $books['Java']['Level'] . "<br />"; 
+      ?>
+   
+   </body>
+</html>
